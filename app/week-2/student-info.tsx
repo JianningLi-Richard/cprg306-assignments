9d7ca61e434eeae956b79703a122cce3bb6cdba6
JianningLi-Richard <RIchardLi.ljn@gmail.com>
2026-01-20 14:54:52 -0700

    week 2 assignment

---
+++ app/week-2/student-info.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+export default function StudentInfo() {
+  return (
+    <div>
+      <div>Jianning Li</div>
+      <Link href="https://github.com/JianningLi-Richard/cprg306-assignments">Github Link</Link>
+    </div>
+  );
+}
