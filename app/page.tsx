8392997e3c8f4f63c74d62a07363edfb8e2563b9
JianningLi-Richard <RIchardLi.ljn@gmail.com>
2026-01-27 13:41:53 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function HomePage() {
   return (
     <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <a href="/week-2">Go to Week 2</a>
+      <a href="/week-2">Go to Week 2</a><br/>
       <a href="/week-3">Go to Week 3</a>
       
     </main>
