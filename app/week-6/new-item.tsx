7a88f83dfdc16899938a8e8f0e3359138eb06584
JianningLi-Richard <RIchardLi.ljn@gmail.com>
2026-02-24 13:20:02 -0700

    Update new-item.tsx

--- app/week-6/new-item.tsx
+++ app/week-6/new-item.tsx
@@ -50,12 +50,10 @@ export default function NewItem({ onAddItem }: NewItemProps) {
                     className={`w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 ${(name === "" || name.length < 2)&& nameTouched ? "border-2 border-red-500" : "border-gray-300"}`}
                     placeholder="Enter item name"
                 />
-                {name === "" && nameTouched && (
+                {(name === "" || name.length < 2 ) && nameTouched && (
                     <p className="text-red-400 text-sm mt-1">Name is required</p >
                 )}
-                {name.length < 2 && nameTouched && (
-                    <p className="text-red-400 text-sm mt-1">Name must be at least 2 characters long</p >
-                )}
+                
             </div>
 
             <div className="mb-4">
