c2961e5deba9aba711a78bd2183b3925d806494e
JianningLi-Richard <RIchardLi.ljn@gmail.com>
2026-03-10 14:03:44 -0600

    add jump to week-7

--- app/week-8/page.tsx
+++ app/week-8/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useUserAuth } from "./_utils/auth-context";
+import Link from "next/link";
 
 export default function Page() {
   const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
@@ -8,14 +9,17 @@ export default function Page() {
   return (
     <div>
       {user ? (
-        <p>
-          Welcome, {user.displayName} ({user.email})
-        </p>
+        <>
+          <p>
+            Welcome, {user.displayName} ({user.email})
+          </p>
+          <button onClick={() => firebaseSignOut()}>Sign out</button><br/>
+          <Link href="/week-7">
+            <button>Shopping List</button>
+          </Link>
+        </>
       ) : (
         <button onClick={() => gitHubSignIn()}>Sign in with GitHub</button>
-      )}
-      {user && (
-        <button onClick={() => firebaseSignOut()}>Sign out</button>
       )}
     </div>
   );
--- app/week-8/shopping-list/page.tsx
+++ app/week-8/shopping-list/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import ItemList from './item-list';
 import NewItem from './new-item';
 import MealIdeas from './meal-ideas';
 import itemsData from './items.json';
+import { useUserAuth } from "../_utils/auth-context";
+import { useRouter } from 'next/navigation';
 
 interface Item {
   id: string;
@@ -14,8 +16,16 @@ interface Item {
 }
 
 export default function Page() {
+  const { user } = useUserAuth();
+  const router = useRouter();
   const [items, setItems] = useState<Item[]>(itemsData);
   const [selectedItemName, setSelectedItemName] = useState<string>('');
+
+  useEffect(() => {
+    if (!user) {
+      router.push("/week-8");
+    }
+  }, [user, router]);
 
   function handleAddItem(newItem: { name: string; quantity: number; category: string }) {
     const item: Item = {
