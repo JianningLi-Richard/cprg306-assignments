7fe424c6c077c26dc2f5f1c8171aed43fbbdcefe
JianningLi-Richard <RIchardLi.ljn@gmail.com>
2026-03-10 13:37:58 -0600

    Update page.tsx

--- app/week-8/page.tsx
+++ app/week-8/page.tsx
@@ -1,13 +1,22 @@
+"use client";
+
 import { useUserAuth } from "./_utils/auth-context";
- 
-const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
- 
-await gitHubSignIn();
- 
-await firebaseSignOut();
- 
-{user && (
-  <p>
-    Welcome, {user.displayName} ({user.email})
-  </p>
-)}
+
+export default function Page() {
+  const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
+
+  return (
+    <div>
+      {user ? (
+        <p>
+          Welcome, {user.displayName} ({user.email})
+        </p>
+      ) : (
+        <button onClick={() => gitHubSignIn()}>Sign in with GitHub</button>
+      )}
+      {user && (
+        <button onClick={() => firebaseSignOut()}>Sign out</button>
+      )}
+    </div>
+  );
+}
